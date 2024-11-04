f74662e48e982fe598d04106c577f00576e2d77f
Mandeep Das <mandeepdas321@gmail.com>
2024-11-04 17:48:16 +0530

    ui changes 11

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -11,7 +11,7 @@ function PostCard({
     <Link to={`/post/${$id}`}>
         <div className='h-full rounded-md bg-gray-200 p-4 m-4 overflow-hidden flex flex-col justify-between align-center'>
             <div className='w-full justify-center align-center mb-2'>
-                <img src={service.getFilePreview(featuredImage)} alt={title} className='rounded-xl w-96 h-40 object-cover'/>
+                <img src={service.getFilePreview(featuredImage)} alt={title} className='rounded-xl sm:w-full w-60 h-40 object-cover'/>
             </div>
             <h2 className='font-semibold font-mono mb-3'>{title}</h2>
         </div>
