89a489e04a70da170e691bda2c75d6a14a3511da
Mandeep Das <mandeepdas321@gmail.com>
2024-11-04 10:34:30 +0530

    ui changes 3

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Container({ children }) {
-  return <div className="w-full max-w-7xl px-0 md:px-4 align-center items-center mt-7">{children}</div>;
+  return <div className="w-full h-full px-0 md:px-4 align-center items-center mt-7">{children}</div>;
 }
 
 export default Container;
