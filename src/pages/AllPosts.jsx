7cdc418ba7079ecc760665f522c89bc4d60fd205
Mandeep Das <mandeepdas321@gmail.com>
2024-11-04 17:57:02 +0530

    ui change 12

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -15,9 +15,9 @@ function AllPosts() {
   return (
     <div className="py-4 text-center flex justify-center align-center mb-20">
       <Container>
-        <div className="flex flex-wrap justify-start align-center gap-y-10">
+        <div className="flex flex-wrap justify-center md:justify-start align-center gap-y-10">
           {posts.map((post) => (
-            <div key={post.$id} className="sm:w-1/3 md:w-1/4 lg:w-1/5 h-72 mb-5">
+            <div key={post.$id} className="w-1/1 md:w-1/3 lg:w-1/4 xl:1/5 h-72 mb-5">
               <PostCard {...post} />
             </div>
           ))}
